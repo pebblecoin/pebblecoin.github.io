a56d657326e2485986da0317929bfe170d8a377f
pebblecoin <edwinshi85@gmail.com>
2018-05-28 12:52:32 -0700

    Update_tags

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "hello world",
+        "excerpt":"This is the first test to use github to post my personal blog. ","categories": [],
+        "tags": ["misc"],
+        "url": "http://localhost:4000/hello-world/",
+        "teaser":null}]
